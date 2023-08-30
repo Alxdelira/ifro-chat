4af307439c5e0a4716d5e8da87b452617c2f6373
Alexandre <alx.delira@gmail.com>
2023-08-30 17:50:02 -0400

    Testando firebase

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  distDir: 'out', // Define o diretório de saída da construção
 }
 
 module.exports = nextConfig
